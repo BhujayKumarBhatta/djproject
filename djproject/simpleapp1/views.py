96e1d459aafa349ea85f0adff72d4ddfc7b66b37
82822 <82822@ITSSBLRINFRL023.ITC.IN>
2018-03-20 16:36:11 +0530

    a

--- djproject/simpleapp1/views.py
+++ djproject/simpleapp1/views.py
@@ -153,19 +153,16 @@ def openstack_view(request):
     try:
         al = nova.servers.list()
         for s in al:
-          if 'asg_name' in s.metadata and s.metadata['asg_name']=='autoscale_demo_1':
-            for net in s.to_dict()['addresses'][s.networks.keys()[0]]:
-              if net['OS-EXT-IPS:type']=='floating':
-                  floatip = net['addr']
-              else:
-                 fixedip = net['addr']
-              sdict = {'sobj': s, 'floatip': floatip , 'fixedip': fixedip}
-            slist.append(sdict)                 
+            if 'asg_name' in s.metadata and s.metadata['asg_name']=='autoscale_demo_1':
+                for net in s.to_dict()['addresses'][s.networks.keys()[0]]:
+                    if net['OS-EXT-IPS:type']=='floating':
+                        sdict = {'sobj': s, 'fip': net['addr']}
+                        slist.append(sdict)
+                
     except:
         pass
     context = {'slist': slist}
-    #return render(request, 'openstack_view.html', context)
-    return HttpResponse(s)
+    return render(request, 'openstack_view.html', context)
    
 
 class Inventory(ListView):
